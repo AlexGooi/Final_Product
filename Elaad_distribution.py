b6741c02d3f76c2ec7ddb474da7b803139a9d1d7
furstdo <d.s.h.furst@tue.nl>
2024-01-19 12:26:21 +0100

    seaborn for plots

--- Elaad_distribution.py
+++ Elaad_distribution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-##import seaborn as sns 
+import seaborn as sns 
 import scipy.stats as stats
 from scipy.stats import gamma
 import numpy as np
